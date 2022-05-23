4c08daf4353d48901b5361164f9fbe1b979450cb
JordiNodeJS <71975008+JordiNodeJS@users.noreply.github.com>
2022-05-23 10:02:34 +0200

    s6e1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Escena from './components/escena/Escena'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Escena /> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/escena/Escena.jsx
@@ -0,0 +1,8 @@
+import './escena.css'
+const Escena = () => (
+    <p>
+        El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial
+    </p>
+)
+
+export default Escena
